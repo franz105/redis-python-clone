953a410edee3a1b717980b16301450b42db0d8f6
Franz Salisi <fsalisi1@berkeley.edu>
2024-06-20 14:24:47 -0700

    send to client socket

--- app/main.py
+++ app/main.py
@@ -5,9 +5,12 @@ def main():
 
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, client_address = server_socket.accept() # wait for client
 
-    server_socket.send(b"+PONG\r\n")
+    print("server started")
+    client_socket.send(b"+PONG\r\n")
+
+    client_socket.close() # close connection
 
 
 if __name__ == "__main__":
