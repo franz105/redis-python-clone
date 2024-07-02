60e2566c2adf5d724dc39f0e17f0dfd529b72cfe
Franz Salisi <fsalisi1@berkeley.edu>
2024-07-01 18:33:40 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_socket, client_address = server_socket.accept() # wait for client
 
-    print("server started")
-    client_socket.send(b"+PONG\r\n")
-
+    print("connection accepted")
+    while (data := client_socket.recv(1024)):
+        client_socket.send(b"+PONG\r\n")
     client_socket.close() # close connection
 
 
